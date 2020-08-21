8a61925163c989f7506e12c5f58b354ee1916b88
NGO-5111-master-niigata <anothergakuto5111@gmail.com>
2020-08-21 18:41:45 +0900

    debug in 202008212

---
+++ JavaScript/CurrentPosition_orijinal.js
@@ -0,0 +1,42 @@
+<!-- 現在地を取得 -->
+
+navigator.geolocation.getCurrentPosition(success,error);
+
+		<!-- 取得に成功した場合の関数 -->
+
+		function success(position){
+
+			var com = position.coords;
+			var comlng = com.longitude ;
+			var comlat = com.latitude ;
+
+			<!-- 新宿駅周辺の地図読み込み -->
+
+			mapboxgl.accessToken = 'pk.eyJ1IjoibmdvNTExMSIsImEiOiJjanh6Z3l6cmIwMG9tM21uNGtuNnB0NXM2In0.IwFlVev1tMBm-gy5zWiS9Q';
+			var map = new mapboxgl.Map({
+				container: 'map',
+				style: 'mapbox://styles/ngo5111/ckb5wuazy2heu1imwnukcqukh',
+				center: [+ comlng , + comlat] ,
+				zoom: 12
+			});
+		}
+
+
+		<!-- 取得に失敗した場合の関数 -->
+
+		function error(err){
+
+			var comlng = 139.70 ;
+			var comlat = 35.68 ;
+		}
+
+
+<!-- 新宿駅周辺の地図読み込み -->
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibmdvNTExMSIsImEiOiJjanh6Z3l6cmIwMG9tM21uNGtuNnB0NXM2In0.IwFlVev1tMBm-gy5zWiS9Q';
+var map = new mapboxgl.Map({
+	container: 'map',
+	style: 'mapbox://styles/ngo5111/ckb5wuazy2heu1imwnukcqukh',
+	center: [+ comlng , + comlat] ,
+	zoom: 12
+});
