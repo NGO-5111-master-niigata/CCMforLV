f28bc31d4cf09080a3fd1aefe6f0151b581127c7
unknown <gakuto5111@gmail.com>
2021-04-05 19:01:26 +0900

    drawer menyu add

--- JavaScript/LowVisionMap_responsible.js
+++ JavaScript/LowVisionMap_responsible.js
@@ -94,3 +94,26 @@ function toggle(){
 }
 
 
+/* ボックス押したらスライドで操作パネル出現 */
+
+$('.menu-trigger').on('click',function(){
+	if($(this).hasClass('active')){
+		$(this).removeClass('active');
+		$('nav').removeClass('open');
+		$('.overlay').removeClass('open');
+	}
+	else {
+		$(this).addClass('active');
+    	$('nav').addClass('open');
+		$('.overlay').addClass('open');
+    }
+});
+
+$('.overlay').on('click',function(){
+	if($(this).hasClass('open')){
+		$(this).removeClass('open');
+		$('.menu-trigger').removeClass('active');
+		$('nav').removeClass('open');      
+	}
+});
+
