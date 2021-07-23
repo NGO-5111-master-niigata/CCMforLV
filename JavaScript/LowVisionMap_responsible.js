a789874ca62d441c4b36830dd7c8ab5cb41ccce6
unknown <gakuto5111@gmail.com>
2021-07-23 13:27:37 +0900

    fixed to background

--- JavaScript/LowVisionMap_responsible.js
+++ JavaScript/LowVisionMap_responsible.js
@@ -268,17 +268,40 @@ navigator.geolocation.getCurrentPosition(success,error);
 		document.getElementById('Navigation_PC').appendChild(Navigation.onAdd(map));
 
 
-
-
-
-
+		var swatches = document.getElementById('swatches');
+		var layer = document.getElementById('ColoredLayer_PC');
+		var colors = [
+		'#ffffcc',
+		'#a1dab4',
+		'#41b6c4',
+		'#2c7fb8',
+		'#253494',
+		'#fed976',
+		'#feb24c',
+		'#fd8d3c',
+		'#f03b20',
+		'#bd0026'
+		];
+		 
+		colors.forEach(function (color) {
+			var swatch = document.createElement('button');
+			swatch.style.backgroundColor = color;
+			swatch.addEventListener('click', function () {
+				map.setPaintProperty(layer.value, 'fill-color', color);
+				});
+			swatches.appendChild(swatch);
+		});
 
 
 /* PC用ここまで */
 
 
-/* スマートフォン用 */
 
+
+
+
+
+/* スマートフォン用 */
 
 
 	/* ハンバーガーメニュー押したらスライドで操作パネル出現 */
